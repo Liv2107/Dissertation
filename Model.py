fece02e0a78645024eb5f94802bc401f06d55936
Olivia Emms <123oliviaemms123@gmail.com>
2024-11-30 00:22:21 +0000

    All datasets added and read by pandas.

--- Model.py
+++ Model.py
@@ -3,5 +3,10 @@ import numpy as np
 import cv2
 
 shades = pd.read_csv('datasets/FoundationShades/allCategories.csv')
+FSCD = pd.read_csv('datasets/SkinTones/FSCD_data.csv')
+SSCC = pd.read_csv('datasets/SkinTones/SSCC_data.csv')
+CCSG = pd.read_csv('datasets/SkinTones/CCSG_data.csv')
 
-print(shades.to_string())
+# global photo
+
+# photo = cv2.cvtColor(photo, cv2.COLOR_BGR2HSV)
