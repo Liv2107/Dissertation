70a51bc9d02122eaf217901354b511a58220368d
Olivia Emms <123oliviaemms123@gmail.com>
2025-01-21 01:18:56 +0000

    Removed skin tone datasets. Implemented my own database using SQL to store images inputted by the camera.

--- Model.py
+++ Model.py
@@ -3,9 +3,6 @@ import numpy as np
 import cv2
 
 shades = pd.read_csv('datasets/FoundationShades/allCategories.csv')
-FSCD = pd.read_csv('datasets/SkinTones/FSCD_data.csv')
-SSCC = pd.read_csv('datasets/SkinTones/SSCC_data.csv')
-CCSG = pd.read_csv('datasets/SkinTones/CCSG_data.csv')
 
 # global photo
 
